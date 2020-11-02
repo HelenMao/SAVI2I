1a7cd1277d3f9fe7fe3ba313a23c6ce042b4804f
PrincessMQ <happygirl199505@163.com>
2020-11-02 18:33:32 +0800

    update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -P models
-wget https://drive.google.com/file/d/1relOFLfOW0ACpr_u6DXgll7Qf6sLSstx -P ./models/Yosemite
+wget https://drive.google.com/file/d/1relOFLfOW0ACpr_u6DXgll7Qf6sLSstx/ -P ./models/Yosemite
 wget https://drive.google.com/file/d/1B1G_Ml-a0phvBG_ePlbwBOpT4hOk9X9h/ -P ./models/Photo2artwork
 wget https://drive.google.com/file/d/1x0sRX-QTQ3z5Eep-ROmX9wcAMHWIBT6j/ -P ./models/CelebAHQ
-wget https://drive.google.com/open?id=1tnDDolN-OMLG4BUNB6rPIjSXoP2FbXgw -P ./models/AFHQ
+wget https://drive.google.com/file/d/19hsK63GJyT_qaqAwaE8mM_zBWncYtBzB/ -P ./models/AFHQ
